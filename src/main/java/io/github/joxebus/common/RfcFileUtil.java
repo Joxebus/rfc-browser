f3043de17c976a336921bcbbcb97b75bc2406bbb
Omar Bautista <joxebus@gmail.com>
2022-04-26 17:35:28 -0500

    Implement custom exception when reading File directory for RFC files

--- src/main/java/io/github/joxebus/common/RfcFileUtil.java
+++ src/main/java/io/github/joxebus/common/RfcFileUtil.java
@@ -4,11 +4,15 @@ import java.io.File;
 import java.io.FilenameFilter;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 
 import io.github.joxebus.bean.RfcResponse;
 import io.github.joxebus.bean.RfcTextLine;
+import io.github.joxebus.exception.RfcException;
 
 public final class RfcFileUtil {
+
+    private static final String TXT_EXTENSION = ".txt";
 
     public static RfcResponse textLinesToResponse(final List<RfcTextLine> textLines, final String filename) {
         RfcResponse rfcResponse = new RfcResponse();
@@ -18,13 +22,13 @@ public final class RfcFileUtil {
     }
 
     public static List<String> getFileNames(final String directory) {
-        FilenameFilter onlyTxtFilter = new FilenameFilter() {
-            @Override
-            public boolean accept(File dir, String name) {
-                return name.endsWith(".txt");
-            }
-        };
-        File folder = new File(directory);
-        return Arrays.asList(folder.list(onlyTxtFilter));
+        try {
+            FilenameFilter onlyTxtFilter = (dir, name) -> name.endsWith(TXT_EXTENSION);
+            File folder = new File(directory);
+            return List.of(Objects.requireNonNull(folder.list(onlyTxtFilter)));
+        } catch (Exception e) {
+            throw new RfcException(String.format("There was a problem with the directory [%s]", directory));
+        }
+
     }
 }
---
+++ src/main/java/io/github/joxebus/exception/RfcException.java
@@ -0,0 +1,8 @@
+package io.github.joxebus.exception;
+
+public class RfcException extends RuntimeException {
+
+    public RfcException(String message) {
+        super(message);
+    }
+}
