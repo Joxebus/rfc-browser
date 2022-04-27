b07d85ea078fb8d428dd2b331b34153025959689
Omar Bautista <joxebus@gmail.com>
2022-04-26 21:32:41 -0500

    Add readFileTextLines to RfcFileUtil

--- src/main/java/io/github/joxebus/common/RfcFileUtil.java
+++ src/main/java/io/github/joxebus/common/RfcFileUtil.java
@@ -1,8 +1,11 @@
 package io.github.joxebus.common;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.FilenameFilter;
-import java.util.Arrays;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
@@ -12,7 +15,7 @@ import io.github.joxebus.exception.RfcException;
 
 public final class RfcFileUtil {
 
-    private static final String TXT_EXTENSION = ".txt";
+    public static final String TXT_EXTENSION = ".txt";
 
     public static RfcResponse textLinesToResponse(final List<RfcTextLine> textLines, final String filename) {
         RfcResponse rfcResponse = new RfcResponse();
@@ -30,5 +33,23 @@ public final class RfcFileUtil {
             throw new RfcException(String.format("There was a problem with the directory [%s]", directory));
         }
 
+    }
+
+    public static List<String> readFileTextLines(final String directory, final String filename) {
+        List<String> fileTextLines = new ArrayList<>();
+        try {
+            File file = new File(directory, filename);
+            FileReader fileReader = new FileReader(file);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            String line = bufferedReader.readLine();
+            while (Objects.nonNull(line)) {
+                fileTextLines.add(line);
+                line = bufferedReader.readLine();
+            }
+            bufferedReader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return fileTextLines;
     }
 }
