8339618aad37a9465187601a580b83d238756c17
Omar Bautista <joxebus@gmail.com>
2022-04-26 21:39:10 -0500

    Modify refactor RfcService to use modern Java statements

--- src/main/java/io/github/joxebus/service/RfcService.java
+++ src/main/java/io/github/joxebus/service/RfcService.java
@@ -1,92 +1,69 @@
 package io.github.joxebus.service;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
+import static io.github.joxebus.common.RfcFileUtil.TXT_EXTENSION;
+
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
+import java.util.stream.IntStream;
 
 import org.springframework.stereotype.Service;
 
 import io.github.joxebus.bean.RfcResponse;
 import io.github.joxebus.bean.RfcTextLine;
 import io.github.joxebus.common.RfcFileUtil;
+import io.github.joxebus.exception.RfcException;
 
 @Service
 public class RfcService {
 
     private static final String RFC_DIR_PATH = System.getenv("RFC_DIR_PATH");
+    public static final String EMPTY_STRING = "";
 
     private List<String> rfcFilenames;
 
     public List<RfcResponse> findFilesWithText(final String text) {
-
         List<RfcResponse> responses = new ArrayList<>();
         for (String filename: getRfcFilenames()) {
-
-            RfcResponse rfcResponse = new RfcResponse();
-            rfcResponse.setFilename(filename);
-
-            List<RfcTextLine> textLines = new ArrayList<>();
-            List<String> fileTextLines = getFileTextLines(new File(RFC_DIR_PATH, filename));
-            for (int i = 0; i < fileTextLines.size(); i++) {
-                String line = fileTextLines.get(i).toLowerCase();
-                if (line.contains(text)) {
-                    RfcTextLine rfcTextLine = new RfcTextLine();
-                    rfcTextLine.setLine(i + 1);
-                    rfcTextLine.setText(fileTextLines.get(i));
-                    textLines.add(rfcTextLine);
-                    rfcResponse.setContent(textLines);
+            try {
+                RfcResponse rfcResponse = getRfcTextLinesAsResponse(filename, text);
+                if (!rfcResponse.getContent().isEmpty()) {
+                    responses.add(rfcResponse);
                 }
+            } catch (RfcException rfcException) {
+                rfcException.printStackTrace();
             }
-
-            if (!textLines.isEmpty())
-                responses.add(rfcResponse);
         }
         return responses;
     }
 
-    public RfcResponse getRfcTextLinesAsResponse(String filename) {
-        List<RfcTextLine> rfcTextLines = getRfcTextLines(filename);
+    public RfcResponse getRfcTextLinesAsResponse(final String filename) {
+        return getRfcTextLinesAsResponse(filename, EMPTY_STRING);
+    }
+
+    public RfcResponse getRfcTextLinesAsResponse(final String filename, final String filter) {
+        List<RfcTextLine> rfcTextLines = getRfcTextLines(filename, filter);
         return RfcFileUtil.textLinesToResponse(rfcTextLines, filename);
     }
 
-    public List<RfcTextLine> getRfcTextLines(String filename) {
-        List<String> filenameList = getRfcFilenames();
+    public List<RfcTextLine> getRfcTextLines(final String filename, final String filter) {
 
-        if (filenameList.contains(filename.concat(".txt")))
-            return null;
-
-        List<RfcTextLine> rfcTextLines = new ArrayList<>();
-        List<String> fileTextLines = getFileTextLines(new File(RFC_DIR_PATH, filename));
-
-
-        for (int i = 0; i < fileTextLines.size(); i++) {
-            RfcTextLine rfcTextLine = new RfcTextLine();
-            rfcTextLine.setLine(i + 1);
-            rfcTextLine.setText(fileTextLines.get(i));
-            rfcTextLines.add(rfcTextLine);
+        if(Objects.isNull(filename) || getRfcFilenames().contains(filename.concat(TXT_EXTENSION))) {
+            return Collections.emptyList();
         }
-        return rfcTextLines;
-    }
 
-    private List<String> getFileTextLines(File file) {
-        List<String> fileTextLines = new ArrayList<>();
-        try {
-            FileReader fileReader = new FileReader(file);
-            BufferedReader bufferedReader = new BufferedReader(fileReader);
-            String line = bufferedReader.readLine();
-            while (line != null) {
-                fileTextLines.add(line);
-                line = bufferedReader.readLine();
-            }
-            bufferedReader.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return fileTextLines;
+        List<String> fileTextLines = RfcFileUtil.readFileTextLines(RFC_DIR_PATH, filename);
+
+        // Since Java 1.8 we can use Streams to iterate.
+        return IntStream.range(0, fileTextLines.size())
+                .filter( index -> fileTextLines.get(index).contains(filter))
+                .mapToObj( index -> {
+                    RfcTextLine rfcTextLine = new RfcTextLine();
+                    rfcTextLine.setLine(index + 1);
+                    rfcTextLine.setText(fileTextLines.get(index));
+                    return rfcTextLine;
+                }).toList();
     }
 
 
